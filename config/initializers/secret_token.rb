045fb04f856d7342876f8e8a5420cb6cb3288ee7
Mayoa Yakob <mayoa.net@gmail.com>
2013-10-01 23:52:11 +0100

    Generate app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ibacca::Application.config.secret_key_base = '6ed5a94dda30a3a8460c9cc0cb1da16b0312ee664e77bfb0a60ee3094e7b69af11a1015aa8c96158edb58797ed644efa6a5d732c8b801ef8f4981d3ca6c60dc1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ibacca::Application.config.session_store :cookie_store, key: '_ibacca_session'
